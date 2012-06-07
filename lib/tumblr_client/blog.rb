3aaf57c7ad14d1dcc7304905b92be8c731766c30
Ryan Cook <cookrn@gmail.com>
2012-06-06 18:46:02 -0600

    support instance level consumer key for the two endpoints that require it

--- lib/tumblr_client/blog.rb
+++ lib/tumblr_client/blog.rb
@@ -1,15 +1,15 @@
 module Tumblr
   class Client
     module Blog
-      
+
       @@standard_options = [:type, :id, :tag, :limit, :offset, :reblog_info, :notes_info, :filter]
       #
       #Gets the info about the blog
       #
-      def blog_info(blog_name)
-        get("v2/blog/#{blog_name}/info", {:api_key => Tumblr::consumer_key})
+      def blog_info( blog_name )
+        get( "v2/blog/#{blog_name}/info" , { :api_key => credentials[ :consumer_key ] } )
       end
-    
+
       #
       #Gets the avatar of specified size
       #Defaults to 64
@@ -17,7 +17,7 @@ module Tumblr
       def avatar(blog_name, size=64)
         get("v2/blog/#{blog_name}/avatar", {:size => size})
       end
-    
+
       #
       # Gets the list of followers for the blog
       #
@@ -29,12 +29,12 @@ module Tumblr
 
       def posts(blog_name, options={})
         url = "v2/blog/#{blog_name}/posts"
-        
+
         if options.has_key?(:type)
           url = "#{url}/#{options[:type]}"
         end
-        
-        params = {:api_key => Tumblr::consumer_key}
+
+        params = { :api_key => credentials[ :consumer_key ] }
         unless options.empty?
           params.merge!(options)
         end
@@ -44,11 +44,11 @@ module Tumblr
       def queue(blog_name)
         get("v2/blog/#{blog_name}/posts/queue", {})
       end
-      
+
       def draft(blog_name)
         get("v2/blog/#{blog_name}/posts/draft", {})
       end
-        
+
       def submissions(blog_name)
         get("v2/blog/#{blog_name}/posts/submission", {})
       end
